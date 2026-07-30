c2fdc4bfc56855bc51f34f2ff235c38ce045943a
Manuel Barbagallo <you@example.com>
2026-07-30 20:19:33 +0200

    Register Sviluppatore and Genere in admin

--- giochi/admin.py
+++ giochi/admin.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 
-from .models import Gioco
+from .models import Genere, Gioco, Sviluppatore
 
 
 @admin.register(Gioco)
 class GiocoAdmin(admin.ModelAdmin):
-    list_display = ("titolo", "piattaforma", "stato", "ore_giocate", "voto")
-    list_filter = ("stato", "piattaforma")
+    list_display = ("titolo", "piattaforma", "stato", "sviluppatore", "ore_giocate", "voto")
+    list_filter = ("stato", "piattaforma", "generi")
     search_fields = ("titolo",)
+    filter_horizontal = ("generi",)
+
+
+@admin.register(Sviluppatore)
+class SviluppatoreAdmin(admin.ModelAdmin):
+    list_display = ("nome", "paese")
+    search_fields = ("nome",)
+
+
+@admin.register(Genere)
+class GenereAdmin(admin.ModelAdmin):
+    list_display = ("nome",)
+    search_fields = ("nome",)
